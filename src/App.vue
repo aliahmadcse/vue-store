e8a917c9c968040a8fbf8e0c0fb0f6e1a34fd7c4
aliahmadcse <aliahmadcse@gmail.com>
2020-05-09 19:24:01 +0500

    Adds car

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app" class="container mt-5">
         <h1>My Shop</h1>
-        <product-list :maximum="maximum" :products="products"></product-list>
+        <product-list :maximum="maximum" :products="products" @add="addItem"></product-list>
     </div>
 </template>
 
@@ -18,6 +18,7 @@ export default {
     data: function() {
         return {
             maximum: 99,
+            cart: [],
             products: null
         };
     },
@@ -27,6 +28,33 @@ export default {
             .then(data => {
                 this.products = data;
             });
+    },
+
+    methods: {
+        /**
+         * Method to add an item in the cart,
+         * Looks, whether the item already exist, if yes, it increment
+         * the quantity
+         * @param product
+         * @returns void
+         */
+        addItem: function(product) {
+            var whichProduct;
+            var existing = this.cart.filter(function(item, index) {
+                if (item.product.id == Number(product.id)) {
+                    whichProduct = index;
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+
+            if (existing.length) {
+                this.cart[whichProduct].qty++;
+            } else {
+                this.cart.push({ product: product, qty: 1 });
+            }
+        }
     }
 };
 </script>
