1a7555bb96d1b8df5f52d4a474c68e732b4371bb
aliahmadcse <aliahmadcse@gmail.com>
2020-05-09 23:24:14 +0500

    Adds routes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App.vue";
 import "bootstrap";
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -10,8 +11,26 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 library.add(faShoppingCart, faDollarSign);
 
+// setting up routing for application
+import Products from "./components/Products";
+import Checkout from "./components/Checkout";
+Vue.use(VueRouter);
+const router = new VueRouter({
+  routes: [
+    {
+      path: "*",
+      component: Products,
+    },
+    {
+      path: "/checkout",
+      component: Checkout,
+    },
+  ],
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
+  router,
 }).$mount("#app");
