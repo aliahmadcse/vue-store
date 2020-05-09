38c31a0e946d06db28dca2d7ea62cccbe276a63f
aliahmadcse <aliahmadcse@gmail.com>
2020-05-09 18:54:07 +0500

    Adds product-list component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <template>
-  <div id="app" class="container mt-5">
-    <h1>My Shop</h1>
-    <p class="animate__animated animate__fadeInRight">Take a look at our offerings below</p>
-    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
-    <price value="4.52"></price>
-  </div>
+    <div id="app" class="container mt-5">
+        <h1>My Shop</h1>
+        <product-list :maximum="maximum" :products="products"></product-list>
+    </div>
 </template>
 
 <script>
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import Price from "./components/Price.vue";
+// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import ProductList from "./components/ProductList.vue";
 
 export default {
-  name: "App",
-  components: {
-    FontAwesomeIcon,
-    Price
-  }
+    name: "App",
+    components: {
+        // FontAwesomeIcon,
+        ProductList
+    },
+    data: function() {
+        return {
+            maximum: 99,
+            products: null
+        };
+    },
+    mounted: function() {
+        fetch("https://hplussport.com/api/products/order/price")
+            .then(response => response.json())
+            .then(data => {
+                this.products = data;
+            });
+    }
 };
 </script>
